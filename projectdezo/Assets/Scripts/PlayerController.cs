0c6782d4e1d41ebba17924755bdc550baab56599
Destiny1811 <b00153707@mytudublin.ie>
2024-08-30 05:00:31 +0200

    final
    adding new features

---
+++ projectdezo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float speed = 30;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 20;
+    
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
+
+
+
